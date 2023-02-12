fa7952b1841cf81f35ef2d10eea4038334b7223d
James Hughes <hughesjames94@gmail.com>
2023-02-12 10:35:56 +0000

    Commit to fix git

--- AI-Robot-FYP--master/ProceduralCity/Assets/Scripts/FPSController.cs
+++ AI-Robot-FYP--master/ProceduralCity/Assets/Scripts/FPSController.cs
@@ -34,36 +34,36 @@ public class FPSController : MonoBehaviour
 
         Vector3 m_Input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
-       // human.MovePosition(transform.position + m_Input * Time.deltaTime * m_Speed);
+        //human.MovePosition(transform.position + m_Input * Time.deltaTime * m_Speed);
 
-       characterController.SimpleMove(m_Input * m_Speed);
+      // characterController.SimpleMove(m_Input * m_Speed);
     }
 
     
 
     // Update is called once per frame
-    // void Update()
-    // {
-    //     transform.Translate(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * speed);
+    void Update()
+    {
+        transform.Translate(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * speed);
 
-    //     transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * rotSpeed, 0);
+        transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * rotSpeed, 0);
 
-    //     if (Input.GetKeyDown(KeyCode.Space))
-    //     {
-    //         human.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-    //         jumpcount += 1;
-    //     }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            human.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            jumpcount += 1;
+        }
 
-    //     if(jumpcount == 1 && spincount < degree / spinspeed)
-    //     {
-    //         transform.Rotate(Vector3.down * spinspeed);
-    //         spincount += 1;
-    //     }
-    //     else
-    //     {
-    //         jumpcount = 0;
-    //         spincount = 0;
-    //     }
+        if(jumpcount == 1 && spincount < degree / spinspeed)
+        {
+            transform.Rotate(Vector3.down * spinspeed);
+            spincount += 1;
+        }
+        else
+        {
+            jumpcount = 0;
+            spincount = 0;
+        }
 
-    // }
+    }
 }
